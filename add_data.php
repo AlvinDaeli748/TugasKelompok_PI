23ef0e723e4daac2d9be919116ba55a5b6a07b9a
Alvin Daeli <alvindaeli748@gmail.com>
2021-04-05 19:28:12 +0700

    Menambahkan timestamp untuk menyimpan waktu presensi

--- add_data.php
+++ add_data.php
@@ -2,16 +2,16 @@
     require_once 'Connection/conn.php';
     $nama = $_POST['nama'];
     $nim = $_POST['nim'];
-    
+    $timestamp = date("Y-m-d H:i:s");
 
     if($db){
-        $sql = "INSERT INTO data (nama,nim) VALUES ('$nama','$nim')";
+        $sql = "INSERT INTO data (nama,nim,timestamp) VALUES ('$nama','$nim','$timestamp')";
         $db->exec($sql);
         echo "<script>
                     alert('Presensi berhasil!'); 
                     window.location.replace('index.php');
                 </script>";
     } elseif(isset($error)){
-        echo "Maaf terjadi kesalahan\nError : $error";
+        echo "Maaf, terjadi kesalahan\nError : $error";
     }
 ?>
